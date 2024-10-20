38d85a561a691d940512dd899258251933def7f6
kaveh <kaveh1999nazari@gmail.com>
2024-10-20 18:26:51 +0330

    return to stable state

--- app/DataTransferObject/BaseDTO.php
+++ app/DataTransferObject/BaseDTO.php
@@ -4,18 +4,7 @@ namespace App\DataTransferObject;
 
 abstract class BaseDTO {
     final public static function fromArray(array $data): static {
-        $reflection = new \ReflectionClass(static::class);
-        $instance = $reflection->newInstanceWithoutConstructor();
 
-        foreach ($data as $property => $value) {
-            if ($reflection->hasProperty($property)) {
-                $prop = $reflection->getProperty($property);
-                $prop->setAccessible(true);
-                $prop->setValue($instance, $value);
-            }
-        }
-
-        return $instance;
     }
 }
 
