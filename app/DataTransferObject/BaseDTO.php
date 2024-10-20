ab7b1c3fe8a44fe06cbad609632c27e380254a9a
kaveh <kaveh1999nazari@gmail.com>
2024-10-20 16:25:33 +0330

    test

--- app/DataTransferObject/BaseDTO.php
+++ app/DataTransferObject/BaseDTO.php
@@ -6,7 +6,15 @@ abstract class BaseDTO
 {
     final public static function fromArray(array $data): self
     {
+        $reflection = new \ReflectionClass(static::class);
+        $properties = $reflection->getProperties();
+        $args = [];
 
+        foreach ($properties as $property) {
+            $name = $property->getName();
+            $args[] = $data[$name] ?? null;
+        }
+
+        return $reflection->newInstanceArgs($args);
     }
-
 }
