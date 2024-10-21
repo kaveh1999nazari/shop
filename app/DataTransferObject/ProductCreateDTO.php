8caa08eab316cccf564c2d83ad31a81fa5bc616c
kaveh <kaveh1999nazari@gmail.com>
2024-10-21 15:04:56 +0330

    add property and change in variables

--- app/DataTransferObject/ProductCreateDTO.php
+++ app/DataTransferObject/ProductCreateDTO.php
@@ -2,14 +2,19 @@
 
 namespace App\DataTransferObject;
 
+use Illuminate\Http\UploadedFile;
+
+/**
+ * @property UploadedFile[] $images
+ */
 class ProductCreateDTO extends BaseDTO {
     public function __construct(
         public string $name,
         public int $category_id,
         public array $options,
         public int $price,
-        public ?string $description = null,
-        public ?array $images = null,
+        public string $description = '',
+        public array $images = [],
     ) {}
 }
 
