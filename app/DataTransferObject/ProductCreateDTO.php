80a94bd38f3de3cfd97d7d771cf72f6e2066e067
kaveh <kaveh1999nazari@gmail.com>
2024-10-21 10:59:16 +0330

    add public for each item

--- app/DataTransferObject/ProductCreateDTO.php
+++ app/DataTransferObject/ProductCreateDTO.php
@@ -4,12 +4,12 @@ namespace App\DataTransferObject;
 
 class ProductCreateDTO extends BaseDTO {
     public function __construct(
-        string $name,
-        ?string $description,
-        ?array $images,
-        int $categoryId,
-        array $options,
-        int $price,
+        public string $name,
+        public ?string $description,
+        public ?array $images,
+        public int $category_id,
+        public array $options,
+        public int $price,
     ) {}
 }
 
