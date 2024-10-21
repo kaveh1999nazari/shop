c9988e4e394cd4f6974acf2c671b1566dbbaafeb
kaveh <kaveh1999nazari@gmail.com>
2024-10-21 13:36:43 +0330

    changes in product DTO

--- app/DataTransferObject/ProductCreateDTO.php
+++ app/DataTransferObject/ProductCreateDTO.php
@@ -5,11 +5,11 @@ namespace App\DataTransferObject;
 class ProductCreateDTO extends BaseDTO {
     public function __construct(
         public string $name,
-        public ?string $description,
-        public ?array $images,
         public int $category_id,
         public array $options,
         public int $price,
+        public ?string $description = null,
+        public ?array $images = null,
     ) {}
 }
 
