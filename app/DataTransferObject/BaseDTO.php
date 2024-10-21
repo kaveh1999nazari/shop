90927f6052070f09c0a4d9fc173265cbadc0b35b
kaveh <kaveh1999nazari@gmail.com>
2024-10-21 10:54:41 +0330

    add reflection

--- app/DataTransferObject/BaseDTO.php
+++ app/DataTransferObject/BaseDTO.php
@@ -3,8 +3,18 @@
 namespace App\DataTransferObject;
 
 abstract class BaseDTO {
-    final public static function fromArray(array $data): static {
-
+    final public static function fromArray(array $data): static
+    {
+        $calledClass = static::class;
+        $reflectionClass = new \ReflectionClass($calledClass);
+        $constructor = $reflectionClass->getConstructor();
+        $parameters = $constructor->getParameters();
+        $args = [];
+        foreach ($parameters as $parameter) {
+            $name = $parameter->getName();
+            $args[] = $data[$name] ?? null;
+        }
+        return new $calledClass(...$args);
     }
 }
 
