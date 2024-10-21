4fb60540e4b28f45ec58bef515d5c5f8a3a75b53
kaveh <kaveh1999nazari@gmail.com>
2024-10-21 13:36:18 +0330

    use static

--- app/DataTransferObject/BaseDTO.php
+++ app/DataTransferObject/BaseDTO.php
@@ -5,16 +5,7 @@ namespace App\DataTransferObject;
 abstract class BaseDTO {
     final public static function fromArray(array $data): static
     {
-        $calledClass = static::class;
-        $reflectionClass = new \ReflectionClass($calledClass);
-        $constructor = $reflectionClass->getConstructor();
-        $parameters = $constructor->getParameters();
-        $args = [];
-        foreach ($parameters as $parameter) {
-            $name = $parameter->getName();
-            $args[] = $data[$name] ?? null;
-        }
-        return new $calledClass(...$args);
+          return new static(...$data);
     }
 }
 
