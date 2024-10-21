edae4a05e75404845e26e5e71aec6dcba885cd8b
kaveh <kaveh1999nazari@gmail.com>
2024-10-21 09:35:15 +0330

    remove some code

--- app/DataTransferObject/ProductCreateDTO.php
+++ app/DataTransferObject/ProductCreateDTO.php
@@ -3,27 +3,13 @@
 namespace App\DataTransferObject;
 
 class ProductCreateDTO extends BaseDTO {
-    public string $name;
-    public ?string $description;
-    public ?array $images;
-    public int $categoryId;
-    public array $options;
-    public int $price;
-
     public function __construct(
-        string $name = '',
-        ?string $description = null,
-        ?array $images = null,
-        int $categoryId = 0,
-        array $options = [],
-        int $price = 0
-    ) {
-        $this->name = $name;
-        $this->description = $description;
-        $this->images = $images;
-        $this->categoryId = $categoryId;
-        $this->options = $options;
-        $this->price = $price;
-    }
+        string $name,
+        ?string $description,
+        ?array $images,
+        int $categoryId,
+        array $options,
+        int $price,
+    ) {}
 }
 
