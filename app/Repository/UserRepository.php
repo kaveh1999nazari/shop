45b41209ba51c1aa1a409ae136d46317ca8f138b
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-12 16:08:34 +0330

    create UserRepository

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -5,9 +5,11 @@ namespace App\Repository;
 use App\Models\User;
 
 
-class UserRepository{
-    public function Register(array $data){
+class UserRepository
+{
+    public function Register(array $data)
+    {
         return User::query()
             ->create($data);
     }
-};
+}
