855c6b6dafa98c090acad312c129fe10d948be07
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-12 14:05:25 +0330

    add new route for UserManagement

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Authentication\AuthenticationController;
 use App\Http\Controllers\Cart\CartController;
 use App\Http\Controllers\Category\CategoryController;
 use App\Http\Controllers\Options\OptionController;
@@ -9,11 +9,16 @@ use App\Http\Controllers\Order\OrderController;
 use App\Http\Controllers\Product\Option\ProductOptionController;
 use App\Http\Controllers\Product\ProductController;
 use App\Http\Controllers\Product\Price\ProductPriceController;
+use App\Http\Controllers\UserManagement\UserManagementController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AuthController::class)->group(function(){
+Route::controller(AuthenticationController::class)->group(function(){
     Route::post('/auth/register', 'register');
     Route::post('/auth', 'login');
+});
+
+Route::controller(UserManagementController::class)->group(function(){
+    Route::post('/register', 'register');
 });
 
 Route::controller(CategoryController::class)->group(function(){
@@ -55,4 +60,4 @@ Route::controller(OrderController::class)->group(function(){
     Route::get('/order', 'listOrder');
 });
 
-Route::post('/product2', [\App\Http\Controllers\Product2Controller::class, 'createProduct2']);
+//Route::post('/product2', [\App\Http\Controllers\Product2Controller::class, 'createProduct2']);
