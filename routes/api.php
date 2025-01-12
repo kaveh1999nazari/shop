40ac301f95bb608c196082dedfdec2f3db681a94
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-12 16:08:03 +0330

    create UserResidentController Route

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Product\Option\ProductOptionController;
 use App\Http\Controllers\Product\ProductController;
 use App\Http\Controllers\Product\Price\ProductPriceController;
 use App\Http\Controllers\UserManagement\UserController;
+use App\Http\Controllers\UserManagement\UserResidentController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthenticationController::class)->group(function(){
@@ -19,6 +20,10 @@ Route::controller(AuthenticationController::class)->group(function(){
 
 Route::controller(UserController::class)->group(function(){
     Route::post('/register', 'register');
+});
+
+Route::controller(UserResidentController::class)->group(function (){
+    Route::post('/register/residents', 'createResident');
 });
 
 Route::controller(CategoryController::class)->group(function(){
