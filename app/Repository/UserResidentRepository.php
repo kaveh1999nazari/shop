c0e73e7277fa6e36f212a829b072f8098a04b1e4
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-12 16:09:18 +0330

    create UserResident Repository

---
+++ app/Repository/UserResidentRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+use App\Models\City;
+use App\Models\UserResident;
+
+class UserResidentRepository
+{
+    public function create(array $data): UserResident
+    {
+        return UserResident::query()
+            ->create($data);
+    }
+
+    public function checkProvinceCityMatch(int $provinceId, int $cityId): bool
+    {
+        return City::query()
+            ->where('id', $cityId)
+            ->where('province_id', $provinceId)
+            ->exists();
+    }
+
+}
