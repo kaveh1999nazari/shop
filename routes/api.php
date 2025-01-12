4ff5406d54e0f9b1fdbdc5c2d4ea3aa4df9146c1
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-12 14:56:23 +0330

    add UserController in route

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Order\OrderController;
 use App\Http\Controllers\Product\Option\ProductOptionController;
 use App\Http\Controllers\Product\ProductController;
 use App\Http\Controllers\Product\Price\ProductPriceController;
-use App\Http\Controllers\UserManagement\UserManagementController;
+use App\Http\Controllers\UserManagement\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthenticationController::class)->group(function(){
@@ -17,7 +17,7 @@ Route::controller(AuthenticationController::class)->group(function(){
     Route::post('/auth', 'login');
 });
 
-Route::controller(UserManagementController::class)->group(function(){
+Route::controller(UserController::class)->group(function(){
     Route::post('/register', 'register');
 });
 
