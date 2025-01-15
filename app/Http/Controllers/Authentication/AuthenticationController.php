e94578de8e2d3f882357ffe7dda35a821c2a38c4
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-15 10:11:07 +0330

    create verifyEmail action for AuthenticationController

--- app/Http/Controllers/Authentication/AuthenticationController.php
+++ app/Http/Controllers/Authentication/AuthenticationController.php
@@ -2,15 +2,39 @@
 
 namespace App\Http\Controllers\Authentication;
 
+use App\Exceptions\NotExistEmail;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\UserRegisterRequest;
-use App\Repository\UserRepository;
+use App\Http\Requests\AuthVerifyEmailRequest;
+use App\Notifications\sendOtpNotification;
 use App\Service\AuthService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
 
 class AuthenticationController extends Controller
 {
-    function __construct(private readonly AuthService $authService) {}
+    function __construct(private readonly AuthService $authService)
+    {
+    }
+
+    /**
+     * @throws NotExistEmail
+     */
+    public function verifyEmail(AuthVerifyEmailRequest $request): JsonResponse
+    {
+        if ($this->authService->checkEmailExist($request->email) === true) {
+
+            $user = $this->authService->findUserByEmail($request->email);
+
+            Notification::send($user, new sendOtpNotification("123456"));
+
+            return response()->json([
+                'message' => 'your OTP Code has been sent to your email.',
+            ]);
+        } else {
+            throw new NotExistEmail();
+        }
+    }
 
     public function login(Request $request)
     {
