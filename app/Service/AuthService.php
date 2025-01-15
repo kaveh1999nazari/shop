177a4f4fded99634b51e0faf12696135cd3d65f9
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-15 10:11:34 +0330

    create checkEmailExist and findUserByEmail action for AuthService

--- app/Service/AuthService.php
+++ app/Service/AuthService.php
@@ -3,17 +3,35 @@
 namespace App\Service;
 
 
-class AuthService{
+use App\Repository\UserManagement\UserRepository;
+use Illuminate\Http\JsonResponse;
 
-    public function login(array $data)
+class AuthService
+{
+
+    public function __construct(private readonly UserRepository $userRepository)
     {
-        if (! $token = auth('api')->attempt($data)) {
+    }
+
+    public function checkEmailExist(string $email): bool
+    {
+        return $this->userRepository->checkUserByEmail($email);
+    }
+
+    public function findUserByEmail(string $email): ?\App\Models\User
+    {
+        return $this->userRepository->findUser($email);
+    }
+
+    public function login(array $data): JsonResponse
+    {
+        if (!$token = auth('api')->attempt($data)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
         return $this->respondWithToken($token);
     }
 
-    protected function respondWithToken($token)
+    protected function respondWithToken($token): JsonResponse
     {
         return response()->json([
             'access_token' => $token,
